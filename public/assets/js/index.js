f173247ace21dfc2b2beba48ab410d656f1aad6b
Jonathanbtq <botquin.jonathan@yahoo.fr>
2023-05-10 20:56:40 +0200

    Burger menu

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,7 @@
-const btnBrgButton = document.querySelector('.button_nav_brg');
-const divBrgBtn = document.querySelector('.nav-deux-ctn');
-const closeBrg = document.querySelector('.burger_menu_close');
+const burger = document.querySelector('.button_nav_brg');
+const divActive = document.querySelector('.nav-deux-ctn');
 
-function openMenu(){
-    divBrgBtn.style.display = "flex";
-}
-
-function closeMenu(){
-    divBrgBtn.style.display = "none";
-}
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_active');
+    divActive.classList.toggle('nav_deux_active')
+})
